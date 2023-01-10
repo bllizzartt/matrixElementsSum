d38c4d40a5f1433324c6e193d942ccac44be93f7
Chase_Haynes <chase.haynez@gmail.com>
2023-01-10 09:43:37 -0500

    new stuff

--- matrix.js
+++ matrix.js
@@ -1,12 +1,14 @@
 // define variable matrix to an array 
 let matrix = [[0, 1, 1, 2], [0, 5, 0, 0], [2, 0, 3, 3]]
+// initiate a function 
 function solution(matrix) {
-    for(var r=0,j=0;j<matrix[0].length;j++){
-        for(var i=0;i<matrix.length;i++){
+  // run a for loop with variables r and j
+    for(let r=0,j=0;j<matrix[0].length;j++){
+        for(let i=0;i<matrix.length;i++){
           if(matrix[i][j]===0) break
           else r+=matrix[i][j]
         }
     }
     return r
   }
-  
+   
